dcd8ad5a5dc27574702f8bc1402e5a52292872d5
Cesar Menezes <cesarmartinsrj15@live.com>
2023-02-01 15:59:36 -0300

    HTML + CSS basic structures done

---
+++ js/calculadora.js
@@ -0,0 +1,13 @@
+var calc = document.querySelector("#calculadora");
+
+var display = calc.querySelector(".calc-display");
+
+var botao = calc.querySelectorAll(".botao");
+
+console.log(botao);
+
+var temp = null;
+
+botao[0].addEventListener("click",function(){
+
+})
